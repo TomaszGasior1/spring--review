5afa46540e9015d7a143946680b41221571d802a
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-06-12 19:17:32 +0200

    Initial commit

---
+++ src/main/java/com/tomaszgasior/spring/App.java
@@ -0,0 +1,20 @@
+package com.tomaszgasior.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/tomaszgasior/spring/beans/beans.xml");
+		
+		Person person = (Person) context.getBean("person");
+		person.speak();
+
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+
+}
---
+++ src/main/java/com/tomaszgasior/spring/Person.java
@@ -0,0 +1,8 @@
+package com.tomaszgasior.spring;
+
+public class Person {
+	public void speak(){
+		System.out.println("hello i am a person");
+	}
+
+}
