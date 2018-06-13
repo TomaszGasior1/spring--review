496a914a7fce1888413c338a713d2990f50049a9
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-06-13 18:11:29 +0200

    Spring 9 Beans scope

--- src/main/java/com/tomaszgasior/spring/App.java
+++ src/main/java/com/tomaszgasior/spring/App.java
@@ -7,19 +7,19 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/tomaszgasior/spring/beans/beans.xml");
-		
-		Person person = (Person) context.getBean("person");
-		person.speak();
 
-		System.out.println(person);
-		
-		Adress adress = (Adress)context.getBean("adress");
+		Person person1 = (Person) context.getBean("person");
+		Person person2 = (Person) context.getBean("person");
+
+		person1.setTaxId(999);
+		System.out.println(person2);
+
+		Adress adress = (Adress) context.getBean("adress");
 		System.out.println(adress);
-		
-		((ClassPathXmlApplicationContext)context).close();
-	}
 
+		((ClassPathXmlApplicationContext) context).close();
+	}
 
 }
