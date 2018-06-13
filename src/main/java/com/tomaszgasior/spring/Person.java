8822976c55a409ea072df14b368e4a52fb07cc59
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-06-13 17:45:51 +0200

    Spring 7 Setting Beans

--- src/main/java/com/tomaszgasior/spring/Person.java
+++ src/main/java/com/tomaszgasior/spring/Person.java
@@ -4,9 +4,15 @@ public class Person {
 
 	private int id;
 	private String name;
-	
-	public Person(){
-		
+
+	private int taxId;
+
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
+	}
+
+	public Person() {
+
 	}
 
 	public Person(int id, String name) {
@@ -14,9 +20,10 @@ public class Person {
 		this.name = name;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId + "]";
 	}
 
 	public void speak() {
